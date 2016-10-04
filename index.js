e84d46152b5fc41ff7cf80668e4e85595a28f15e
Kit <snowfennard@gmail.com>
2016-10-04 19:42:09 +0100

    baseUrl added to constructor with refactor.

--- index.js
+++ index.js
@@ -2,56 +2,82 @@
 
 // Import modules
 const request = require("request");
-const url = require("url");
 const cheerio = require("cheerio");
-// const crypto = require("crypto");
-
-// Constants
-const BASE_URI = "https://moodle2.gla.ac.uk/";
-const LOGIN_PATH = "login/index.php";
-const HOME_PATH = "my/";
-
-
-/*
-function md5Hash(str) {
-    return crypto.createHash("md5").update(str).digest("hex");
-}
-*/
+const url = require("url");
 
 
 class Moodle2 {
-    constructor(username, password, callback) {
-        this.session = request.defaults({ jar: true });
+    constructor(baseUrl, username, password, callback) {
+        if (typeof baseUrl !== "string") {
+            throw new Error("baseUrl must be provided as a string!");
+        }
+
+        let parsedUrl = url.parse(baseUrl);
+        let strippedUrl = parsedUrl.protocol + "//" + parsedUrl.host;  // is it possible to use url.format instead?
+
+        this.baseUrl = strippedUrl;
+        this.session = request.defaults({
+            jar: true,
+            followRedirect: false
+        });
         this.isAuthed = false;
+
+        this.loginPath = null;
+        this.homePath = null;
 
         if (username && password) {
             this.login(username, password, callback);
         }
     }
 
+    // callback(err, success) where success is a bool indicating if login was a success
+    // TODO: Currently success always returns true and does not do error checking.
     login(username, password, callback) {
-        let login_page = url.resolve(BASE_URI, LOGIN_PATH);
-        console.log("Logging in user " + username + "...");
+        if (this.isAuthed) {
+            console.error("Already logged in!");
+            let err = new Error("Already logged in.");
+            return callback(err);
+        }
 
-        let formData = {
-            username: username,
-            password: password
-        };
+        let self = this;
 
-        this.session.post({ url: login_page, formData: formData }, (err, httpResponse, body) => {
-            if (err) {
-                console.error("Error on login: ", err);
-                return callback(err);
-            }
+        if (!this.loginPath) {
+            return this._getRedirectLocation(this.baseUrl, (err, href) => {
+                if (err) {
+                    console.error("Could not get login path!")
+                    callback(err);
+                }
 
-            console.log("Login successful?");  // TODO: check if login was actually successful
-            this.isAuthed = true;
+                self.loginPath = href;
+                return _doLogin();
+            });
+        } else {
+            return _doLogin();
+        }
 
-            return callback(null, this.isAuthed);  // returns true if logged in
-        });
+        function _doLogin() {
+            console.log("Logging in user " + username + "...");
+
+            let formData = {
+                username: username,
+                password: password
+            };
+
+            self.session.post({ url: self.loginPath, formData: formData }, (err, httpResponse, body) => {
+                if (err) {
+                    console.error("Error on login: ", err);
+                    return callback(err);
+                }
+
+                console.log("Login successful?");  // TODO: check if login was actually successful
+                self.isAuthed = true;
+
+                return callback(null, self.isAuthed);  // returns true if logged in
+            });
+        }
     }
 
-    // Returns list of courses
+    // callback(err, courses) where courses is a list of course objects
     getCourses(callback) {
         if (!this.isAuthed) {
             console.error("Do login first!");
@@ -59,29 +85,45 @@ class Moodle2 {
             return callback(err);
         }
 
-        let homeURL = url.resolve(BASE_URI, HOME_PATH);
+        let self = this;
 
-        this.session.get({ url: homeURL }, (err, httpResponse, body) => {
-            if (err) {
-                console.error("Error in getCourses: " + err);
-                return callback(err);
-            }
+        if (!this.homePath) {
+            return this._getRedirectLocation(this.baseUrl, (err, href) => {
+                if (err) {
+                    console.error("Could not get path to home page!");
+                    callback(err)
+                }
 
-            let $ = cheerio.load(body);
-            let courseElements = $("div .course_title a");
-            let courses = [];
-
-            courseElements.each((i, elem) => {
-                let courseElement = $(elem);
-                courses[i] = {
-                    title: courseElement.text(),
-                    url: courseElement.attr("href")
-                };
+                self.homePath = href;
+                return _doGetCourses();
             });
+        } else {
+            return _doGetCourses();
+        }
 
-            console.log("Course list obtained!");
-            callback(null, courses);
-        });
+        function _doGetCourses() {
+            self.session.get({ url: self.homePath }, (err, httpResponse, body) => {
+                if (err) {
+                    console.error("Error in getCourses: " + err);
+                    return callback(err);
+                }
+
+                let $ = cheerio.load(body);
+                let courseElements = $("div .course_title a");
+                let courses = [];
+
+                courseElements.each((i, elem) => {
+                    let courseElement = $(elem);
+                    courses[i] = {
+                        title: courseElement.text(),
+                        url: courseElement.attr("href")
+                    };
+                });
+
+                console.log("Course list obtained!");
+                callback(null, courses);
+            });
+        }
     }
 
     // Returns object of {section: [<folder list>]}
@@ -191,8 +233,36 @@ class Moodle2 {
         }
     }
 
-    static downloadFile(fileUrl, destination, callback) {
-        callback(null);
+    // Takes a URL, checkes for status code > 300, then callbacks the location header if it finds it.
+    _getRedirectLocation(url, callback) {
+        return this.session.get({ url: url }, (err, httpResponse) => {
+            if (err) {
+                console.error("Error getting redirect location for url: " + url);
+                callback(err)
+            }
+            
+            let href;
+
+            if (httpResponse.statusCode > 300) {
+                href = httpResponse.headers["location"];
+            }
+
+            // If a location header is not found, return the original url it was called with
+            href = href || url;
+            return callback(null, href);
+        });
+    }
+
+    static getFileEtag(fileUrl, callback) {
+        this.session.head({ url: fileUrl }, (err, httpResponse) => {
+            let headers = httpResponse.headers;
+            return callback(headers["Etag"]);
+        });
+    }
+
+    // Returns a file stream???
+    static streamFile(url) {
+        return this.session(url);
     }
 }
 
